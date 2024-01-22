f508268678eda716d57d5ceaa07e8b0c37052a6b
bruno <bruno.lusardi.96@gmail.com>
2024-01-22 18:54:45 +0100

    Added validator lib, obscure email func, success prop

--- controllers/subscribersController.js
+++ controllers/subscribersController.js
@@ -1,34 +1,56 @@
 const nodemailer = require("nodemailer");
+const validator = require("validator");
 const Subscriber = require("../models/Subscriber");
 const emailSenderConfiguration = require("../config/nodemailer/configEmailTransporter");
 const sendMailToSubscriber = require("../config/nodemailer/sendMail");
+const obscureEmail = require("../config/nodemailer/obscureEmail");
 
 const createSubscriber = async (req, res) => {
   const { name, email } = req.body;
 
-  // still to add email validation
-
   if (!name || !email) {
-    return res.status(400).json({ message: "All fields are required." });
+    return res
+      .status(400)
+      .json({ success: false, message: "All fields are required." });
   }
+
+  // email validation example (basic testing)
+  const emailIsValid = validator.isEmail(email);
+  if (!emailIsValid) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Email is invalid. Try again" });
+  }
+
+  // obscure email
+  let obscuredEmail = obscureEmail(email);
 
   const emailAlreadyExists = await Subscriber.findOne({ email }).lean().exec();
   if (emailAlreadyExists) {
     return res.status(409).json({
-      message: `The email ${email} is already associated with the subscription plan. Choose another one or check for errors.`,
+      success: false,
+      message: `The email <${obscuredEmail}> is already associated with the subscription plan. Choose another one or check for errors.`,
     });
   }
 
   const actualSubscriber = await Subscriber.create({ name, email });
   if (!actualSubscriber) {
     return res.status(400).json({
+      success: false,
       message: "There was an error. Try agaian",
     });
   }
 
   // send email if stored in db
   const transporter = nodemailer.createTransport(emailSenderConfiguration);
-  sendMailToSubscriber(transporter, process.env.EMAIL, email, name, res);
+  sendMailToSubscriber(
+    transporter,
+    process.env.EMAIL,
+    email,
+    obscuredEmail,
+    name,
+    res
+  );
 };
 
 module.exports = createSubscriber;
